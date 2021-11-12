7b4afa3061d48abaa128034dddde482277213e02
chenlong-xcy <chenlonghyl@163.com>
2021-11-12 11:31:12 +0800

    命名管道

--- IPC/IPC/test.cpp
+++ IPC/IPC/test.cpp
@@ -127,3 +127,24 @@ int main()
 }
 
 int pipe2(int pipefd[2], int flags);
+
+int mkfifo(const char *pathname, mode_t mode);
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FILE_NAME "myfifo"
+
+int main()
+{
+	umask(0); //将文件默认掩码设置为0
+	if (mkfifo(FILE_NAME, 0666) < 0){
+		perror("mkfifo");
+		return 1;
+	}
+
+	//create success...
+
+	return 0;
+}
