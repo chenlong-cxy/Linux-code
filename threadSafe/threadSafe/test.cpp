21422cf775b351aa4b7212ec4b949b809d8c2720
chenlong-xcy <chenlonghyl@163.com>
2022-02-15 22:05:44 +0800

    Linux线程安全

--- threadSafe/threadSafe/test.cpp
+++ threadSafe/threadSafe/test.cpp
@@ -77,3 +77,60 @@ int main()
 	}
 	return 0;
 }
+
+
+//³õÊ¼»¯»¥³âÁ¿
+int pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//Ïú»Ù»¥³âÁ¿
+int pthread_mutex_destroy(pthread_mutex_t *mutex);
+
+//»¥³âÁ¿¼ÓËø
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_trylock(pthread_mutex_t *mutex);
+
+//»¥³âÁ¿½âËø
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int tickets = 1000;
+pthread_mutex_t mutex;
+void* TicketGrabbing(void* arg)
+{
+	const char* name = (char*)arg;
+	while (1){
+		pthread_mutex_lock(&mutex);
+		if (tickets > 0){
+			usleep(100);
+			printf("[%s] get a ticket, left: %d\n", name, --tickets);
+			pthread_mutex_unlock(&mutex);
+		}
+		else{
+			pthread_mutex_unlock(&mutex);
+			break;
+		}
+	}
+	printf("%s quit!\n", name);
+	pthread_exit((void*)0);
+}
+int main()
+{
+	pthread_mutex_init(&mutex, NULL);
+	pthread_t t1, t2, t3, t4;
+	pthread_create(&t1, NULL, TicketGrabbing, "thread 1");
+	pthread_create(&t2, NULL, TicketGrabbing, "thread 2");
+	pthread_create(&t3, NULL, TicketGrabbing, "thread 3");
+	pthread_create(&t4, NULL, TicketGrabbing, "thread 4");
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	pthread_join(t4, NULL);
+	pthread_mutex_destroy(&mutex);
+	return 0;
+}
