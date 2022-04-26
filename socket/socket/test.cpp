588df42fe3bc8ca7c46a716bf6caf9b2d643a8da
chenlong-xcy <chenlonghyl@163.com>
2022-04-26 17:19:15 +0800

    套接字编程UDP

--- socket/socket/test.cpp
+++ socket/socket/test.cpp
@@ -4,3 +4,14 @@ uint32_t htonl(uint32_t hostlong);
 uint16_t htons(uint16_t hostshort);
 uint32_t ntohl(uint32_t netlong);
 uint16_t ntohs(uint16_t netshort);
+
+
+int socket(int domain, int type, int protocol);
+
+int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+int listen(int sockfd, int backlog);
+
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
