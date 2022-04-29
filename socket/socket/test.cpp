f5801b7e172d2372406b9e4531a7079a478dfee9
chenlong-xcy <chenlonghyl@163.com>
2022-04-29 23:05:29 +0800

    UDP套接字编程

--- socket/socket/test.cpp
+++ socket/socket/test.cpp
@@ -15,3 +15,55 @@ int listen(int sockfd, int backlog);
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 
 int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+//创建套接字
+int socket(int domain, int type, int protocol);
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define DEFAULT 8081
+
+class UdpServer
+{
+public:
+	UdpServer(int port = DEFAULT)
+		: _port(port)
+		, _sockfd(-1)
+	{};
+	bool InitServer()
+	{
+		//创建套接字
+		_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+		if (_sockfd < 0){ //创建套接字失败
+			std::cerr << "socket error" << std::endl;
+			return false;
+		}
+
+		std::cout << "socket create success, sockfd: " << _sockfd << std::endl;
+		
+		return true;
+	}
+	~UdpServer()
+	{};
+private:
+	int _port; //端口号
+	std::string _ip; //IP地址
+	int _sockfd; //文件描述符
+};
+
+#include "udp_server.hpp"
+
+int main()
+{
+	UdpServer* svr = new UdpServer();
+	svr->InitServer();
+	return 0;
+}
+
+//绑定
+int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
