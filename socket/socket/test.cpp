21958e813e6dc8439fd8e52c222e412e2f570eaf
chenlong-xcy <chenlonghyl@163.com>
2022-04-30 22:53:55 +0800

    UDP套接字编程

--- socket/socket/test.cpp
+++ socket/socket/test.cpp
@@ -67,3 +67,10 @@ int main()
 
 //绑定
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+//字符串IP转整数IP
+in_addr_t inet_addr(const char *cp);
+
+ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+
