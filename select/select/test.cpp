05fe8c8ef2cee59a4060a1f45bfa5bbd5c5c950a
chenlong-xcy <chenlonghyl@163.com>
2022-08-10 20:31:23 +0800

    IO多路转接select

--- select/select/test.cpp
+++ select/select/test.cpp
@@ -5,8 +5,8 @@ int  FD_ISSET(int fd, fd_set *set);    //用来测试描述词组set中相关fd的位是否为真
 void FD_SET(int fd, fd_set *set);      //用来设置描述词组set中相关fd的位
 void FD_ZERO(fd_set *set);             //用来清除描述词组set的全部位
 
-
 fd_set readset;
 FD_SET(fd, &readset);
 select(fd + 1, &readset, NULL, NULL, NULL);
 if (FD_ISSET(fd, readset)){...}
+
